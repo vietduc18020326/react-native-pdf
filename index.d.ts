703555febcfdee441557d46bf7eaf222aca80635
Duc Pham <duc.pham01@platform.inc>
2023-03-24 17:21:06 +0700

    add

---
+++ index.d.ts
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2017-present, Wonday (@wonday.org)
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import * as React from 'react';
+import * as ReactNative from 'react-native';
+
+export type TableContent = {
+    children: TableContent[],
+    mNativePtr: number,
+    pageIdx: number,
+    title: string,
+};
+
+interface Props {
+    style?: ReactNative.StyleProp<ReactNative.ViewStyle>,
+    source: object,
+    page?: number,
+    scale?: number,
+    minScale?: number,
+    maxScale?: number,
+    horizontal?: boolean,
+    spacing?: number,
+    password?: string,
+    activityIndicator?: any,
+    activityIndicatorProps?: object,
+    enableAntialiasing?: boolean,
+    enablePaging?: boolean,
+    enableRTL?: boolean,
+    enableAnnotationRendering?: boolean,
+    fitPolicy?: number,
+    trustAllCerts?: boolean,
+    singlePage?: boolean,
+    onLoadProgress?: (percent: number,) => void,
+    onLoadComplete?: (numberOfPages: number, path: string, size: {height: number, width: number}, tableContents?: TableContent[]) => void,
+    onPageChanged?: (page: number, numberOfPages: number) => void,
+    onError?: (error: object) => void,
+    onPageSingleTap?: (page: number, x: number, y: number) => void,
+    onScaleChanged?: (scale: number) => void,
+    onPressLink?: (url: string) => void,
+    onScroll?: (event: ReactNative.NativeSyntheticEvent<ReactNative.NativeScrollEvent>) => void;
+}
+
+declare class Pdf extends React.Component<Props, any> {
+    setPage: (pageNumber: number) => void;
+}
+
+export default Pdf;
